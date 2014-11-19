e1c49a35c21f05c16d1a589f7706d8548ac7d21f
Aymerick <aymerick@jehanne.org>
2014-11-19 17:09:17 +0100

    Adds all routes

--- app/router.js
+++ app/router.js
@@ -7,9 +7,31 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
+  this.route('logout');
+  this.route('forgotten');
 
-  this.resource('sites', function() {
-    this.route('site', { path: ':site_id' });
+  this.resource('sites', {path: '/'}, function() {
+    this.resource('site', { path: '/sites/:site_id' }, function() {
+      this.resource('posts', {path: '/'}, function () {
+        this.route('post', {path: ':post_id'});
+      });
+
+      this.resource('events', function () {
+        this.route('event', {path: ':event_id'});
+      });
+
+      this.resource('pages', function () {
+        this.route('page', {path: ':page_id'});
+      });
+
+      this.resource('settings', function () {
+        this.route('general');
+
+        this.resource('settings.actions', {path: '/actions'}, function () {
+            this.route('action', {path: ':action_id'});
+        });
+      });
+    });
   });
 });
 
