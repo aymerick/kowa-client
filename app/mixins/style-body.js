5e6cb8edb73a7ba832a2d1ad522bf97b8e97f98d
Aymerick <aymerick@jehanne.org>
2014-11-20 12:22:41 +0100

    Adds style-body

---
+++ app/mixins/style-body.js
@@ -0,0 +1,32 @@
+// mixin used for routes that need to set a css className on the body tag
+// source: https://github.com/TryGhost/Ghost/blob/master/core/client/mixins/style-body.js
+
+var styleBody = Ember.Mixin.create({
+    activate: function () {
+        this._super();
+
+        var cssClasses = this.get('classNames');
+
+        if (cssClasses) {
+            Ember.run.schedule('afterRender', null, function () {
+                cssClasses.forEach(function (curClass) {
+                    Ember.$('body').addClass(curClass);
+                });
+            });
+        }
+    },
+
+    deactivate: function () {
+        this._super();
+
+        var cssClasses = this.get('classNames');
+
+        Ember.run.schedule('afterRender', null, function () {
+            cssClasses.forEach(function (curClass) {
+                Ember.$('body').removeClass(curClass);
+            });
+        });
+    }
+});
+
+export default styleBody;
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,5 +1,8 @@
 import UnauthenticatedRoute from 'kowa/routes/unauthenticated';
+import styleBody from 'kowa/mixins/style-body';
 
-var LoginRoute = UnauthenticatedRoute.extend({});
+var LoginRoute = UnauthenticatedRoute.extend(styleBody, {
+  classNames: ['kowa-login'],
+});
 
 export default LoginRoute;
