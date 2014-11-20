f81892e4c1d5b4f28a8aa9e653535164db85119d
Aymerick <aymerick@jehanne.org>
2014-11-20 17:51:44 +0100

    Starts to handle auth errors

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 import SimpleAuthApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-var ApplicationRoute = Ember.Route.extend(SimpleAuthApplicationRouteMixin);
+var ApplicationRoute = Ember.Route.extend(SimpleAuthApplicationRouteMixin, {
+  actions: {
+    sessionAuthenticationFailed: function (error) {
+      // this.controllerFor('application').set('loginErrorMessage', error.message);
+
+      // if (error.errors) {
+      //     this.notifications.showErrors(error.errors);
+      // } else {
+      //     // connection errors don't return proper status message, only req.body
+      //     this.notifications.showError('There was a problem on the server.');
+      // }
+    }
+  }
+});
 
 export default ApplicationRoute;
