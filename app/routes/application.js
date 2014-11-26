416a1c5bebf07b7a8b9440b44a3cc0486f0cbf80
Aymerick <aymerick@jehanne.org>
2014-11-26 10:51:59 +0100

    Cleanuop

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,18 +2,16 @@ import Ember from 'ember';
 import SimpleAuthApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 var ApplicationRoute = Ember.Route.extend(SimpleAuthApplicationRouteMixin, {
-  actions: {
-    sessionAuthenticationFailed: function(error) {
-      // this.controllerFor('application').set('loginErrorMessage', error.message);
-
-      // if (error.errors) {
-      //     this.notifications.showErrors(error.errors);
-      // } else {
-      //     // connection errors don't return proper status message, only req.body
-      //     this.notifications.showError('There was a problem on the server.');
-      // }
-    }
-  }
+  // actions: {
+  //   sessionAuthenticationFailed: function(error) {
+  //     if (error.errors) {
+  //         this.notifications.showErrors(error.errors);
+  //     } else {
+  //         // connection errors don't return proper status message, only req.body
+  //         this.notifications.showError('There was a problem on the server.');
+  //     }
+  //   }
+  // }
 });
 
 export default ApplicationRoute;
