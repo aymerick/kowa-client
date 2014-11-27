b2160eec7cbea2552cd835a166924ee18d703875
Aymerick <aymerick@jehanne.org>
2014-11-28 00:16:18 +0100

    Changes /api/users/me into /api/me - Starts work on /api/users/{user_id}/sites/{site_id} endpoints

--- app/services/auth_session.js
+++ app/services/auth_session.js
@@ -1,9 +1,24 @@
 import Ember from 'ember';
 import SimpleAuthSession from 'simple-auth/session';
+import ajax from 'ic-ajax';
 
 var AuthSession = SimpleAuthSession.extend({
+  // adds currentUser property to simple-auth session
   currentUser: Ember.computed(function () {
-    return this.container.lookup('store:main').find('user', 'me');
+    var self = this;
+
+    var userPromise = ajax('/api/me').then(function(response) {
+      var store = self.container.lookup('store:main');
+
+      // push user to store
+      var user = store.push('user', response.user);
+
+      return user;
+    });
+
+    return Ember.ObjectProxy.extend(Ember.PromiseProxyMixin).create({
+      promise: userPromise
+    });
   })
 });
 
