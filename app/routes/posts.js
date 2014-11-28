150707e4ddea3500ae9384ac7171e09140a893b5
Aymerick <aymerick@jehanne.org>
2014-11-28 17:12:58 +0100

    WIP on pagination

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,10 +3,11 @@ import AuthenticatedRoute from 'kowa/routes/authenticated';
 var PostsRoute = AuthenticatedRoute.extend({
   model: function() {
     var site = this.modelFor('site');
-    var selector = { 'site': site.get('id'), 'page': 1 };
+    var selector = { 'site': site.get('id'), 'page': 1, 'per_page': 10 };
 
     // using filter() allows the template to auto-update when new models are pulled in from the server
     return this.store.filter('post', selector, function () {
+      // nothing to filter
       return true;
     });
   }
