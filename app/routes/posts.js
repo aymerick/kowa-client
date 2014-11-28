f81405020045222cefbd2490036614cbb78a33f3
Aymerick <aymerick@jehanne.org>
2014-11-28 16:01:25 +0100

    Uses JSON API endpoints, because we can't paginate ember-data relationships :/

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,8 +1,14 @@
 import AuthenticatedRoute from 'kowa/routes/authenticated';
 
 var PostsRoute = AuthenticatedRoute.extend({
-  model: function(params) {
-    return this.modelFor('site').get('posts');
+  model: function() {
+    var site = this.modelFor('site');
+    var selector = { 'site': site.get('id'), 'page': 1 };
+
+    // using filter() allows the template to auto-update when new models are pulled in from the server
+    return this.store.filter('post', selector, function () {
+      return true;
+    });
   }
 });
 
