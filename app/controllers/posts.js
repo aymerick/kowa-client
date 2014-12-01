ec6adedd93b0362eb65f2d5658f12f1c80a53a87
Aymerick <aymerick@jehanne.org>
2014-12-01 17:48:18 +0100

    Sorts posts

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import PaginationControllerMixin from 'kowa/mixins/pagination-controller';
 
 var PostsController = Ember.ArrayController.extend(PaginationControllerMixin, {
-
+    sortProperties: ['createdAt'],
 });
 
 export default PostsController;
