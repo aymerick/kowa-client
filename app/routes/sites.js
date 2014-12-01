0f2df6e686c468642f3560f88c4250d3c805ff9d
Aymerick <aymerick@jehanne.org>
2014-12-01 16:02:03 +0100

    Fixes redirect when accessing / endpoint

--- app/routes/sites.js
+++ app/routes/sites.js
@@ -7,8 +7,13 @@ var SitesRoute = AuthenticatedRoute.extend({
     });
   },
 
-  afterModel: function(sites /* , transition */) {
-    this.transitionTo('site', sites.get('firstObject'));
+  afterModel: function(sites, transition, queryParams) {
+    // catch when accessing "/" endpoint
+    if (!transition.params.site) {
+      // redirects to first site
+      // @todo Handle case when user have no site yet !
+      this.transitionTo('site', sites.get('firstObject'));
+    }
   }
 });
 
