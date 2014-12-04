9336e09b89b1aa31ed6f3d7984f209bc840f5e30
Aymerick <aymerick@jehanne.org>
2014-12-04 15:08:57 +0100

    Use more idiomatic ember api

--- app/serializers/site.js
+++ app/serializers/site.js
@@ -1,13 +1,10 @@
 import DS from 'ember-data';
 
+var SITE_SERIALIZER_FIELDS = [ 'name', 'tagline', 'description', 'moreDesc', 'joinText' ];
+
 var SiteSerializer = DS.RESTSerializer.extend({
   serialize: function(site, options) {
-    var fields = [ 'name', 'tagline', 'description', 'moreDesc', 'joinText' ];
-    var result = { };
-
-    fields.forEach(function(field) {
-      result[field] = site.get(field);
-    });
+    var result = site.getProperties(SITE_SERIALIZER_FIELDS);
 
     if (options && options.includeId) {
       result['id'] = site.get('id');
