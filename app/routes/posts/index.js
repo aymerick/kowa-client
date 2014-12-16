ca8bc278e8482fa23d9f15cc0f6436304211a830
Aymerick <aymerick@jehanne.org>
2014-12-16 15:40:17 +0100

    Fixes a typo

--- app/routes/posts/index.js
+++ app/routes/posts/index.js
@@ -1,7 +1,7 @@
 import AuthenticatedRoute from 'kowa/routes/authenticated';
 
 var PostsIndexRoute = AuthenticatedRoute.extend({
-  // redirects /:site_id/posts to to /:site_id/posts/:post_id
+  // redirects /:site_id/posts to /:site_id/posts/:post_id
   beforeModel: function () {
     var posts = this.modelFor('posts');
     var firstPost = posts.get('firstObject');
