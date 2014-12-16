c44791cdd2ecf25a2548e78b10018e48226c1935
Aymerick <aymerick@jehanne.org>
2014-12-16 14:40:01 +0100

    Removes dead code

--- app/views/post-item-view.js
+++ app/views/post-item-view.js
@@ -1,9 +1,6 @@
 import ActiveItemView from 'kowa/views/active-item';
 
 var PostItemView = ActiveItemView.extend({
-    click: function () {
-        this.get('controller').send('showPostContent');
-    }
 });
 
 export default PostItemView;
