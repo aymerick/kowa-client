e2b724d62a99ecc5251e93cae3a0f4c15a162a77
Aymerick <aymerick@jehanne.org>
2015-01-16 16:17:39 +0100

    Reverts edition of settings/general if save button was not clicked

--- app/routes/settings/general.js
+++ app/routes/settings/general.js
@@ -3,6 +3,17 @@ import AuthenticatedRoute from 'kowa/routes/authenticated';
 var SettingsGeneralRoute = AuthenticatedRoute.extend({
   model: function() {
     return this.modelFor('site');
+  },
+
+  deactivate: function () {
+    var model = this.modelFor('settings.general');
+
+    // revert any unsaved changes on exit
+    if (model && model.get('isDirty')) {
+        model.rollback();
+    }
+
+    this._super();
   }
 });
 
