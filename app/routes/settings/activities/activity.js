cb421a1974f87eba321bd65865231909f0cf19e0
Aymerick <aymerick@jehanne.org>
2015-01-16 16:19:01 +0100

    WIP on WIP on settings/activities

--- app/routes/settings/activities/activity.js
+++ app/routes/settings/activities/activity.js
@@ -3,6 +3,17 @@ import AuthenticatedRoute from 'kowa/routes/authenticated';
 var SettingsActivitiesActivityRoute = AuthenticatedRoute.extend({
   model: function(params) {
     return this.store.find('activity', params.activity_id);
+  },
+
+  deactivate: function () {
+    var model = this.modelFor('settings.activities.activity');
+
+    // revert any unsaved changes on exit
+    if (model && model.get('isDirty')) {
+        model.rollback();
+    }
+
+    this._super();
   }
 });
 
