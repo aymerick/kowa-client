0f44b93e956fa50256763d16ca1aa48652818b8b
Aymerick <aymerick@jehanne.org>
2015-01-16 16:26:33 +0100

    Removes auto-save when changing cover/logo on settings/general

--- app/controllers/settings/general.js
+++ app/controllers/settings/general.js
@@ -7,27 +7,16 @@ var SettingsGeneralController = Ember.ObjectController.extend({
     actions: {
         removeLogo: function() {
             this.get('model').set('logo', null);
-            this.send('save');
         },
 
         removeCover: function() {
             this.get('model').set('cover', null);
-            this.send('save');
         },
 
         // called by 'select-image' modal controller
         imageSelected: function(field, image) {
             var model = this.get('model');
             model.set(field, image);
-
-            // save model
-            var self = this;
-            model.save().then(function (siteSaved) {
-                self.get('flashes').success('Saved.');
-                return siteSaved;
-            }).catch(function () {
-                self.get('flashes').danger('Failed to save.');
-            });
         },
 
         save: function () {
