3a6384d7901b3d7e8bd3038e16df841517182979
Aymerick <aymerick@jehanne.org>
2015-01-17 11:21:37 +0100

    Caches model for activities.index route

--- app/routes/settings/activities/index.js
+++ app/routes/settings/activities/index.js
@@ -5,11 +5,16 @@ var SettingsActivitiesIndexRoute = AuthenticatedRoute.extend({
   model: function() {
     var site = this.modelFor('site');
 
-    // using filter() allows the template to auto-update when new models are pulled in from the server
-    return this.store.filter('activity', { 'site': site.get('id') }, function () {
+    // 1. Caching resultPromise permits to avoid sending a request to server everytime we transition into this route
+    // 2. Using filter() allows the template to auto-update when new models are pulled in from the server
+    var resultPromise = this.get('resultPromise') || this.store.filter('activity', { 'site': site.get('id') }, function () {
       // nothing to filter
       return true;
-    });
+    })
+
+    this.set('resultPromise', resultPromise);
+
+    return resultPromise;
   }
 });
 
