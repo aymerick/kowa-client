44cb228eddadc2a70dc46fbbf8bf1327f1284dbd
Aymerick <aymerick@jehanne.org>
2015-01-17 11:27:22 +0100

    Caches result of store#filter() calls on pages/posts/images to avoid unecessary network requests

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -12,11 +12,16 @@ var PagesRoute = AuthenticatedRoute.extend({
 
     this.set('paginationParams', params);
 
-    // using filter() allows the template to auto-update when new models are pulled in from the server
-    return this.store.filter('page', params, function () {
+    // 1. Caching resultPromise permits to avoid sending a request to server everytime we transition into this route
+    // 2. Using filter() allows the template to auto-update when new models are pulled in from the server
+    var resultPromise = this.get('resultPromise') || this.store.filter('page', params, function () {
       // nothing to filter
       return true;
     });
+
+    this.set('resultPromise', resultPromise);
+
+    return resultPromise;
   },
 
   setupController: function (controller, model) {
