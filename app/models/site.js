f8f34659f659eb96d0a236ca766176f045728bae
Aymerick <aymerick@jehanne.org>
2015-02-03 18:24:02 +0100

    Adds BuildMaster

--- app/models/site.js
+++ app/models/site.js
@@ -2,6 +2,8 @@ import DS from 'ember-data';
 
 var Site = DS.Model.extend({
   createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date'),
+  builtAt: DS.attr('date'),
 
   name: DS.attr(),
   tagline: DS.attr(),
