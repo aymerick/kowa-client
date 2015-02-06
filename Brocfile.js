1323cd984d78488daea8a32a0750b6edba0cea25
Aymerick <aymerick@jehanne.org>
2015-02-06 17:50:58 +0100

    Fixes import of a new font-awesome file

--- Brocfile.js
+++ Brocfile.js
@@ -39,6 +39,7 @@ app.import(app.bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.eot", {
 app.import(app.bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
 app.import(app.bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
 app.import(app.bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+app.import(app.bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
 app.import(app.bowerDirectory + "/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
 
 // CodeMirror
