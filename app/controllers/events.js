9543af031be6d13feb421c3ab30b88ef70d4a87d
Aymerick <aymerick@jehanne.org>
2015-02-10 17:37:40 +0100

    Fixes events sorting

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import PaginationControllerMixin from 'kowa/mixins/pagination-controller';
 
 var EventsController = Ember.ArrayController.extend(PaginationControllerMixin, {
-  sortProperties: ['startAt'],
+  sortProperties: ['startDate'],
   sortAscending: false
 });
 
