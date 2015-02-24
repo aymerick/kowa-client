9f5e31a5ed007d86448afc942c885252bb8e5812
Aymerick <aymerick@jehanne.org>
2015-02-24 11:04:17 +0100

    Enhances summernote-editor component, adding a fillHeight property

--- app/components/summernote-editor.js
+++ app/components/summernote-editor.js
@@ -1,13 +1,18 @@
 import Ember from "ember";
 
-var SummernoteEditor = Ember.TextArea.extend({
-  height: 300,
+var SummernoteEditor = Ember.Component.extend({
+  classNames: ['summernote-editor'],
+  classNameBindings: ['fillHeight'],
+
+  height: null,
+  fillHeight: false,
+  $window: null,
+  resizeHandler: null,
   placeholder: false,
 
   // initialize summernote when inserted
   initSummernote: function() {
-    this.$().summernote({
-      height: this.get('height'),
+    var settings = {
       placeholder: this.get('placeholder'),
       toolbar: [
         ['style', ['style']],
@@ -17,19 +22,48 @@ var SummernoteEditor = Ember.TextArea.extend({
       ],
       styleTags: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'blockquote'],
       disableDragAndDrop: true,
-      lang: 'fr-FR', // @todo FIXME
+      disableResizeEditor: true,
+      lang: 'fr-FR', // @todo FIXME i18n
       onChange: Ember.run.bind(this, this.onChange)
-    });
+    };
+
+    if (this.get('height') !== null) {
+      settings = Ember.merge(settings, {
+        height: this.get('height'),
+        minHeight: this.get('height')
+      });
+    }
+
+    this.$('textarea').summernote(settings);
+
+    if (this.get('fillHeight')) {
+      this.resizeHandler = Ember.run.bind(this, this.onResize);
+
+      this.$window = Ember.$(window);
+      this.$window.on("resize", this.resizeHandler);
+      this.resizeHandler();
+    }
   }.on('didInsertElement'),
 
   // remove summernote when destroyed
   removeSummernote: function(){
-    this.$().destroy();
+    if (this.resizeHandler !== null) {
+      this.$window.off("resize", this.resizeHandler);
+    }
+
+    this.$('textarea').destroy();
   }.on('willDestroyElement'),
 
   // callback when content changed
   onChange: function(contents) {
-    this.set('value', contents);
+    this.set('content', contents);
+  },
+
+  // callback when window size changed
+  onResize: function() {
+    var topBarHeight = this.$('.note-editor .note-toolbar').outerHeight();
+
+    this.$('.note-editor .note-editable').css('top', topBarHeight);
   }
 });
 
