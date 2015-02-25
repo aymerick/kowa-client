67d83a103ee402e0a5ff4d60761ed71604687e0c
Aymerick <aymerick@jehanne.org>
2015-02-25 10:59:46 +0100

    Fixes pages.new and events.new routes

--- app/routes/events/new.js
+++ app/routes/events/new.js
@@ -2,17 +2,17 @@ import AuthenticatedRoute from 'kowa/routes/authenticated';
 import ContentEditionRoute from 'kowa/mixins/content-edition-route';
 
 var EventsNewRoute = AuthenticatedRoute.extend(ContentEditionRoute, {
-  // use EventEditController
-  controllerName: 'event.edit',
+  // use EventsEventController
+  controllerName: 'events.event',
 
   // this is a fresh new model
   model: function() {
     return this.store.createRecord('event', { site: this.modelFor('site') });
   },
 
-  // use 'event/edit' template
+  // use event' template
   renderTemplate: function() {
-    this.render('event/edit');
+    this.render('events/event');
   }
 });
 
--- app/routes/pages/new.js
+++ app/routes/pages/new.js
@@ -2,17 +2,17 @@ import AuthenticatedRoute from 'kowa/routes/authenticated';
 import ContentEditionRoute from 'kowa/mixins/content-edition-route';
 
 var PagesNewRoute = AuthenticatedRoute.extend(ContentEditionRoute, {
-  // use PageEditController
-  controllerName: 'page.edit',
+  // use PagesPageController
+  controllerName: 'pages.page',
 
   // this is a fresh new model
   model: function() {
     return this.store.createRecord('page', { site: this.modelFor('site') });
   },
 
-  // use 'page/edit' template
+  // use page template
   renderTemplate: function() {
-    this.render('page/edit');
+    this.render('pages/page');
   }
 });
 
