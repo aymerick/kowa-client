fbe42a79633de2886e4dec3e71440948acb670cf
Aymerick <aymerick@jehanne.org>
2015-02-27 16:08:47 +0100

    Adds missing translations

--- app/controllers/modals/delete-page.js
+++ app/controllers/modals/delete-page.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ContentDeleteModalMixin from 'kowa/mixins/content-delete-modal';
 
 var DeletePageModal = Ember.ObjectController.extend(ContentDeleteModalMixin, {
-  deleteMsgOk: 'page.delete', // This is an i18n key
+  deleteMsgOk: 'page.deleted', // This is an i18n key
   deleteMsgFail: 'page.deleteFailed', // This is an i18n key
 
   // i18n for attributes values and components parameters
--- app/controllers/modals/delete-post.js
+++ app/controllers/modals/delete-post.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ContentDeleteModalMixin from 'kowa/mixins/content-delete-modal';
 
 var DeletePostModal = Ember.ObjectController.extend(ContentDeleteModalMixin, {
-  deleteMsgOk: 'post.delete', // This is an i18n key
+  deleteMsgOk: 'post.deleted', // This is an i18n key
   deleteMsgFail: 'post.deleteFailed', // This is an i18n key
 
   // i18n for attributes values and components parameters
