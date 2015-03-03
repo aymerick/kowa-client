b13f8f92f9af1776fcb43352aa4348761cd28669
Aymerick <aymerick@jehanne.org>
2015-03-03 09:50:46 +0100

    Redirects /:site_id to /:site_id/settings if site description is blank

--- app/routes/site/index.js
+++ app/routes/site/index.js
@@ -1,9 +1,16 @@
+import Ember from 'ember';
 import AuthenticatedRoute from 'kowa/routes/authenticated';
 
 var SiteIndexRoute = AuthenticatedRoute.extend({
   afterModel: function () {
-    // redirects /:site_id/ to /:site_id/posts
-    this.transitionTo('posts');
+    var site = this.modelFor('site');
+    if (Ember.isBlank(site.get('description'))) {
+      // redirects /:site_id/ to /:site_id/settings
+      this.transitionTo('settings');
+    } else {
+      // redirects /:site_id/ to /:site_id/posts
+      this.transitionTo('posts');
+    }
   }
 });
 
