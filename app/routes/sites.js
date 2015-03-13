995aade20959444e5da6929eef563b7c59cd9157
Aymerick <aymerick@jehanne.org>
2015-03-13 11:06:32 +0100

    Wires application locale to current user locale

--- app/routes/sites.js
+++ app/routes/sites.js
@@ -1,8 +1,20 @@
+import Ember from 'ember';
 import AuthenticatedRoute from 'kowa/routes/authenticated';
 
 var SitesRoute = AuthenticatedRoute.extend({
   model: function() {
+    var self = this;
+
     return this.session.get('currentUser').then(function(currentUser){
+      // setup locale
+      var application = self.container.lookup('application:main');
+      application.set('locale', currentUser.get('lang'));
+
+      // observers locale change
+      Ember.addObserver(currentUser, 'lang', function() {
+        application.set('locale', currentUser.get('lang'));
+      });
+
       return currentUser.get('sites');
     });
   },
