22e380a6fa52ca633557d078bbeb84bd3dcebb28
Aymerick <aymerick@jehanne.org>
2015-04-01 16:24:27 +0200

    Makes initializer testable

--- app/initializers/lang.js
+++ app/initializers/lang.js
@@ -1,19 +1,18 @@
 import LangService from 'kowa/services/lang';
 
-var LangInitializer = {
+export function initialize(container, application) {
+  application.register('langService:main', LangService);
+
+  application.inject('controller', 'langService', 'langService:main');
+  application.inject('route', 'langService', 'langService:main');
+
+  // setup locale
+  var langService = container.lookup('langService:main');
+  application.set('locale', langService.get('currentLang'));
+}
+
+export default {
   name: 'injectLangService',
   after: 't', // ember-cli-i18n
-
-  initialize: function (container, application) {
-    application.register('langService:main', LangService);
-
-    application.inject('controller', 'langService', 'langService:main');
-    application.inject('route', 'langService', 'langService:main');
-
-    // setup locale
-    var langService = container.lookup('langService:main');
-    application.set('locale', langService.get('currentLang'));
-  }
+  initialize: initialize
 };
-
-export default LangInitializer;
