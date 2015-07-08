ec4750eb69d358dc37c1f308ed1ef0cfa197cba5
Aymerick <aymerick@jehanne.org>
2015-07-08 18:11:36 +0200

    Uses /api/members/order endpoint to update members order

--- app/controllers/settings/members/index.js
+++ app/controllers/settings/members/index.js
@@ -3,18 +3,35 @@ import Ember from 'ember';
 var SettingsMembersController = Ember.ArrayController.extend({
   sortProperties: ['order', 'createdAt'],
 
+  needs: ['settings'],
+  site: Ember.computed.alias('controllers.settings.model'),
+
   actions: {
     reorderItems: function(newOrder) {
+      var ids = [ ];
       var order = 1;
 
       newOrder.forEach(function(member){
+        ids.push(member.get('id'));
+
         member.set('order', order);
-        // @todo Remove that individual save
-        member.save();
         order++;
       });
 
-      // @todo Save all orders with a single request
+      var url = '/api/members/order?site=' + this.get('site').get('id');
+      var self = this;
+
+      Ember.$.ajax({
+        url:         url,
+        type:        'PUT',
+        data:        JSON.stringify(ids),
+        dataType:    'json',
+        contentType: 'application/json'
+      }).then(function(/* response */) {
+        // NOOP
+      }, function(/*xhr , status, error */) {
+        Ember.get(self, 'flashMessages').danger('Failed to save members order.');
+      });
     }
   },
 
